31d92f9b02bb91008297a3d896daeedd874eda93
Chaten Mundle <chetanmundle7875@gmail.com>
2024-02-13 14:44:36 +0530

    test change

--- src/main/java/com/api/resturentapplication/ResturentapplicationApplication.java
+++ src/main/java/com/api/resturentapplication/ResturentapplicationApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResturentapplicationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResturentapplicationApplication.class, args); 
+//		hello this is new
 	}
 }
